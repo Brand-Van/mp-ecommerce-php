60f478f19c67471e45b681b1093136d4553ecd69
agusarone <agustinaronec@gmail.com>
2020-07-23 02:11:16 -0300

    prueba3333

--- payment.php
+++ payment.php
@@ -1,4 +1,11 @@
 <?php
-https://api.mercadopago.com/v1/payments/[ID]?access_token=[TEST-1312814065114980-072304-1ee125809f1817194f120ad82a80d679-614098036]
+
+require ('mercadopago.php');
+
+$mp = new MP ("ENV_ACCESS_TOKEN");
+
+$payment = $mp->get(
+    "/v1/payments/". $paymentId
+);
 
 ?>
